06457d62d8f65d2c9abcd53218b84abc8a93a547
Jonathan Merritt <j.s.merritt@gmail.com>
2019-07-20 16:51:28 +1000

    Bat symbol test

--- src/Main.hs
+++ src/Main.hs
@@ -45,6 +45,23 @@ bezierThingo
     ]
 
 
+batSymbol :: Path Float
+batSymbol
+  = Path (mkP 290 319)
+    [ CurveTo (mkP 277 248) (mkP 232 197) (mkP 201 257)
+    , CurveTo (mkP 172 185) (mkP 96 218) (mkP 162 291)
+    , CurveTo (mkP 22 244) (mkP 11 98) (mkP 194 66)
+    , CurveTo (mkP 134 136) (mkP 264 207) (mkP 261 52)
+    , LineTo (mkP 273 73)
+    , LineTo (mkP 308 73)
+    , LineTo (mkP 320 52)
+    , CurveTo (mkP 317 207) (mkP 447 136) (mkP 387 66)
+    , CurveTo (mkP 570 98) (mkP 559 244) (mkP 419 291)
+    , CurveTo (mkP 485 218) (mkP 409 185) (mkP 380 257)
+    , CurveTo (mkP 349 197) (mkP 304 248) (mkP 290 319)
+    ]
+
+
 inCapitalY :: Image S Y Word8
 inCapitalY = Massiv.makeArray Seq (Sz (512 :. 512)) gen
   where
@@ -61,7 +78,16 @@ inBezierThingo = Massiv.makeArray Seq (Sz (512 :. 512)) gen
         pt = mkP (fromIntegral i) (fromIntegral j)
 
 
+inBatSymbol :: Image S Y Word8
+inBatSymbol = Massiv.makeArray Seq (Sz (365 :. 581)) gen
+  where
+    gen (j :. i) = if inPath batSymbol pt then 255 else 0
+      where
+        pt = mkP (0.5 + fromIntegral i) (0.5 + fromIntegral j)
+
+
 main :: IO ()
 main = do
   Massiv.writeImage "test-in-polygon.png" inCapitalY
   Massiv.writeImage "test-in-bezier-thingo.png" inBezierThingo
+  Massiv.writeImage "test-in-bat-symbol.png" inBatSymbol
